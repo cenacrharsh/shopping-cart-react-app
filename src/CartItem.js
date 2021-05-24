7fcdcc48a2bd3d326298370d35b4324780269cb2
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-05-24 17:14:31 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,8 +13,34 @@ class CartItem extends React.Component {
   }
 
   increaseQuantity = () => {
-    console.log(this);
-    console.log(this.state);
+    this.setState(
+      (prevState) => {
+        return {
+          qty: prevState.qty + 1,
+        };
+      },
+      () => {
+        console.log("this.state", this.state);
+      }
+    );
+  };
+
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+    // destructuring
+    if (qty === 0) {
+      return;
+    }
+    this.setState(
+      (prevState) => {
+        return {
+          qty: prevState.qty - 1,
+        };
+      },
+      () => {
+        console.log("this.state", this.state);
+      }
+    );
   };
 
   render() {
@@ -22,12 +48,16 @@ class CartItem extends React.Component {
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} alt="" />
+          <img
+            style={styles.image}
+            alt=""
+            src="https://image.flaticon.com/icons/png/512/2920/2920329.png"
+          />
         </div>
         <div className="right-block">
           <div style={{ font: 25 }}>{title}</div>
-          <div style={{ color: "#777" }}>{price}</div>
-          <div style={{ color: "#777" }}>{qty}</div>
+          <div style={{ color: "#777" }}>Rs:{price}</div>
+          <div style={{ color: "#777" }}>Qty:{qty}</div>
           <div className="cart-item-actions">
             <img
               alt="increase"
@@ -39,6 +69,7 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons"
               src="https://image.flaticon.com/icons/png/512/992/992683.png"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
